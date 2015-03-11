dc20d39d2898f7bb60b40b76aab9ffcefa4a6f84
lutzer <in_the_fishtank@gmx.de>
2015-03-11 19:34:50 +0100

    added clock example

---
+++ clockExample/clockExample.ino
@@ -0,0 +1,26 @@
+#include "OliLedMatrix.h"
+#include "numbers.h"
+
+/* LED MATRIX PINS */
+#define LED_LATCH 4
+#define LED_DATA 6
+#define LED_CLOCK 5
+
+//setup pins for led Matrix
+OliLedMatrix ledMatrix(LED_LATCH,LED_DATA,LED_CLOCK);
+
+void setup() {
+
+}
+
+void loop() {
+  
+  unsigned long seconds = millis() / 1000;
+  
+  ledMatrix.clear();
+  
+  ledMatrix.drawArray(1,2,Numbers[seconds/10],4);
+  ledMatrix.drawArray(5,2,Numbers[seconds%10],4);
+      
+  ledMatrix.display();
+}
--- matrixExample/matrixExample.ino
+++ matrixExample/matrixExample.ino
@@ -18,14 +18,14 @@ void loop() {
   
   ledMatrix.clear();
   
-  if (time%800 < 100)
+  if (time%1000 < 100)
       //draws a rectangle at position 3,3 with height 2 and width 2
       ledMatrix.drawRectangle(3,3,2,2); 
-  else if (time%800 < 200)
+  else if (time%1000 < 200)
       ledMatrix.drawRectangle(2,2,4,4);
-  else if (time%800 < 300)
+  else if (time%1000 < 300)
       ledMatrix.drawRectangle(1,1,6,6);
-  else
+  else if (time%1000 < 300)
       ledMatrix.drawRectangle(0,0,8,8);
       
   ledMatrix.display();
